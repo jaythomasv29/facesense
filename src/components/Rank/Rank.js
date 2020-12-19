9fd973870f9bb1052b537baad3b512c5770f3aec
James Vongampai <jamesvongampai@Jamess-Mac-mini.attlocal.net>
2020-12-19 01:23:49 -0800

    revise css style in imageinputform component

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -2,12 +2,12 @@ const Rank = () => {
   return (
     <div>
     <h4 className="white f1 mt0 mb0">{"facesense"} </h4> 
-      <div className="white f3 mt2">
+      {/* <div className="white f3 mt2">
         {'James, your current rank is...'}
-      </div>
-      <h1 className="white f2 mb0">
+      </div> */}
+      {/* <h1 className="white f2 mb0">
         {'#5'}
-      </h1>
+      </h1> */}
     </div>
   )
 
